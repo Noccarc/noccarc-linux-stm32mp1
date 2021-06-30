a7c0c01af05a71a03b098b4c7835d8cf68619cbf
Nipun Pal <nipun.k@noccarc.com>
2021-06-30 08:05:39 +0530

    Changed clock frequency to 74.25Mhz for correct calculation of the PLL Clock frequency.

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -3194,7 +3194,7 @@ static const struct panel_desc powertip_ph800480t013_idf02  = {
 
 
 static const struct drm_display_mode nvd_tm2257da_mode = {
-	.clock = 72400,
+	.clock = 74250,
 	.hdisplay = 1280,
 	.hsync_start = 1280 + 22,
 	.hsync_end = 1280 + 22 + 160,
