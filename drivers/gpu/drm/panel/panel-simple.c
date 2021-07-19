d0b99b999d0b3fb8281ed178f833662a430dcee1
npal <nipun.k@noccarc.com>
2021-07-19 11:48:11 +0530

    Added changes for the AUO screen tested.

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1475,6 +1475,33 @@ static const struct panel_desc cdtech_s070swv29hg_dc44 = {
 	.connector_type = DRM_MODE_CONNECTOR_DPI,
 };
 
+static const struct drm_display_mode custom_noccarc_mode = {
+	.clock = 66000,
+	.hdisplay = 1024,
+	.hsync_start = 1024 + 100,
+	.hsync_end = 1024 + 100 + 50,
+	.htotal = 1024 + 100 + 220,
+	.vdisplay = 768,
+	.vsync_start = 768 + 20,
+	.vsync_end = 768 + 20 + 8,
+	.vtotal = 768 + 20 + 18,
+};
+
+static const struct panel_desc custom_noccarc = {
+	.modes = &custom_noccarc_mode,
+	.num_modes = 1,
+	.bpc = 6,
+	.size = {
+		.width = 261,
+		.height = 163,
+	},
+	.bus_format = MEDIA_BUS_FMT_RGB666_1X18,
+	.bus_flags = DRM_BUS_FLAG_PIXDATA_SAMPLE_NEGEDGE,
+	.connector_type = DRM_MODE_CONNECTOR_DPI,
+};
+
+
+
 static const struct drm_display_mode cdtech_s070wv95_ct16_mode = {
 	.clock = 35000,
 	.hdisplay = 800,
@@ -4349,6 +4376,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "urt,umsh-8596md-11t",
 		.data = &urt_umsh_8596md_lvds,
+	}, {
+		.compatible = "custom,noccarc",
+		.data = &custom_noccarc,
 	}, {
 		.compatible = "urt,umsh-8596md-19t",
 		.data = &urt_umsh_8596md_lvds,
