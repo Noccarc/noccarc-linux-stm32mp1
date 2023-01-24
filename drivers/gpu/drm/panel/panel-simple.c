d8e4a6a9a6c2e020d02da77497a6ae0f27c1418f
Noccarc <67786566+Noccarc@users.noreply.github.com>
2023-01-24 11:23:22 +0530

    Update panel-simple.c
    updated bus flag to jeilda

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -2956,7 +2956,7 @@ static const struct panel_desc newhaven_nhd_10_1024600af_lsxv_ctp = {
 		.width = 224,
 		.height = 127,
 	},
-	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
+	.bus_format = MEDIA_BUS_FMT_RGB888_1X7X4_JEIDA,
 	.bus_flags = DRM_BUS_FLAG_DE_HIGH,
 	.connector_type = DRM_MODE_CONNECTOR_LVDS,
 };
